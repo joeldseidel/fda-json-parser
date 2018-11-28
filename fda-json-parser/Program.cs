3f0aae3e2321e1661ae1f1cb4c9c18d8e4d14188
Joel Seidel <joeldseidel@gmail.com>
2018-11-28 00:35:11 -0500

    Create basic application

---
+++ fda-json-parser/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace fda_json_parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Joel Seidel, Maverick Systems LLC");
+            Console.WriteLine("Let's parse the FDA's files, shall we?");
+            Console.Write("\nPress any key to continue");
+            Console.ReadKey();
+
+            FDAFileManager fdaFileManager = new FDAFileManager();
+        }
+    }
+}
